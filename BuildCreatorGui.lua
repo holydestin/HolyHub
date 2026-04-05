545af77cd8ccc0b08c59a8b7a66efb2b2f856cd4
holydestin <hdestin4@gmail.com>
2026-04-05 12:04:56 -0500

    Create BuildCreatorGui module to handle GUI-related functions.

---
+++ BuildCreatorGui.lua
@@ -0,0 +1,28 @@
+-- BuildCreatorGui.lua
+-- Module defining functions to manage the GUI for building
+
+local BuildCreatorGui = {}
+
+-- Function to initialize the GUI
+function BuildCreatorGui.initializeGui()
+    -- GUI initialization logic
+end
+
+-- Function to handle stat changes
+function BuildCreatorGui.handleStatChange(stat, value)
+    -- Logic to handle stat changes based on the provided parameters
+end
+
+-- Function to manage selections
+function BuildCreatorGui.manageSelection(selected)
+    -- Logic to manage user selections in the GUI
+end
+
+-- Function to save builds
+function BuildCreatorGui.saveBuild(buildData)
+    -- Logic to save the build data
+end
+
+-- Additional functionality from the original LocalScript can be integrated here
+
+return BuildCreatorGui
